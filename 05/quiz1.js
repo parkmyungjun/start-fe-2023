6bedd62f074c0d3893c6b6f5034fd7b4de5ff996
mj <pmj9498@gmail.com>
2023-04-05 18:11:28 +0900

    5th Quiz

---
+++ 05/quiz1.js
@@ -0,0 +1,9 @@
+
+
+// //
+// const $prev = document.querySelection('.prev');
+// const $next = document.querySelection('.next');
+
+// $prev.addEventListener('click',() =>{
+    
+// })
---
+++ 05/test.js
@@ -0,0 +1,10 @@
+console.log('05');
+
+
+window.addEventListener('click', () => {
+    console.log('window load event1');
+});
+
+window.addEventListener('click', () => {
+    console.log('window load event2');
+});
